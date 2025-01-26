82c144925513dac1152daefec91082e0ae0b1515
dam6 <143343329+dam6@users.noreply.github.com>
2025-01-26 20:15:13 +0100

    Update mempatch.sh

--- mempatch.sh
+++ mempatch.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-mem_range=$(cat /proc/$1/maps | grep heap | awk '{print $1}')
-mem_addr1=$(echo $mem_range | awk -F "-" '{print $1}')
-mem_addr2=$(echo $mem_range | awk -F "-" '{print $2}')
 
-for i in $(./finder $1 $mem_addr1 $mem_addr2 $2); do
-    ./writer $1 $i $3
+for mem_range in $(cat /proc/$1/maps | grep rw | awk '{print $1}'); do
+
+    mem_addr1=$(echo $mem_range | awk -F "-" '{print $1}')
+    mem_addr2=$(echo $mem_range | awk -F "-" '{print $2}')
+
+    for i in $(./finder $1 $mem_addr1 $mem_addr2 $2); do
+        ./writer $1 $i $3
+    done
+
 done
